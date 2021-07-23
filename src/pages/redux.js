91a5b5503200e57215bee114c97c30378c895dd8
351174 <351174@nd.com>
2021-07-23 19:16:23 +0800

    累加器实现

---
+++ src/pages/redux.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import store from "../store";
+export default class Redux extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  add = () => {
+    store.dispatch({ type: "ADD", payload: 1 });
+  };
+  minus = () => {
+    store.dispatch({ type: "MINUS", payload: 1 });
+  };
+  render() {
+    return (
+      <div className="border">
+        <h3>累加器</h3>
+        <button onClick={this.add}>add</button>
+        <span style={{ marginLeft: "10px", marginRight: "10px" }}>
+          {store.getState()}
+        </span>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
+}
